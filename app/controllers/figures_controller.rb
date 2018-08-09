b0fb2d79b63ad7636abdd9604d88fbefb42c1c3b
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-09 19:30:02 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -45,7 +45,7 @@ class FiguresController < ApplicationController
 
   get '/figures/:id/edit' do
     @figure = Figure.find_by_id(params[:id])
-    binding.pry
+    # binding.pry
     @titles = Title.all
     @landmarks = Landmark.all
     erb :'/figures/edit'

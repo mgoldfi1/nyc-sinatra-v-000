c35b98c90407b31c21cb7908b6e52ebd08a3d97c
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-09 18:38:17 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,42 @@
+class FiguresController < ApplicationController
+
+
+
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    figure = Figure.create(name: params[:figure][:name])
+    params[:figure][:title_ids].each do |id|
+      figure.titles << Title.find_by_id(id)
+    end
+    params[:figure][:landmark_ids].each do |id|
+      figure.landmarks << Landmark.find_by_id(id)
+    end
+    if !params[:landmark][:name].empty? && !params[:landmark][:year].empty?
+      figure.landmarks << Landmark.create(name: params[:landmark][:name],year_completed: params[:landmark][:year])
+    end
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
--- config.ru
+++ config.ru
@@ -7,5 +7,9 @@ if defined?(ActiveRecord::Migrator) && ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending run `rake db:migrate` to resolve the issue.'
 end
 
+
+use FiguresController
 use LandmarksController
+
+
 run ApplicationController

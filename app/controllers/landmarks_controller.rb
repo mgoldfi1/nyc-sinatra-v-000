e31e0993ec74837aafbf42e3b2ecdbb0e188202d
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-09 19:52:11 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -15,5 +15,11 @@ class LandmarksController < ApplicationController
   get '/landmarks/:id' do
   @landmark = Landmark.find_by_id(params[:id])
   erb :'/landmarks/show'
-end
+  end
+
+  get '/landmarks/:id/edit' do
+
+  end
+
+
 end

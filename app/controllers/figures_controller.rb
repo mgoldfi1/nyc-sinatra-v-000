bc8de80811ebcbdd1c014226b3e99d6875a57d2c
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-09 19:38:21 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -54,6 +54,25 @@ class FiguresController < ApplicationController
   post '/figures/:id' do
     @figure = Figure.find_by_id(params[:id])
     @figure.update(name: params[:figure][:name])
+    title_ids = params[:figure][:title_ids]
+    landmark_ids = params[:figure][:landmark_ids]
+    titles = []
+    if !title_ids.nil? && !title_ids.empty?
+    title_ids.each do |id|
+      titles << Title.find_by_id(id)
+    end
+  end
+  if !landmark_ids.nil? && !landmark_ids.empty?
+    landmark_ids.each do |id|
+      figure.landmarks << Landmark.find_by_id(id)
+    end
+  end
+    if !params[:landmark][:name].empty?
+      figure.landmarks << Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year])
+    end
+    if !params[:title][:name].empty?
+      figure.titles << Title.create(name: params[:title][:name])
+    end
     @figure.save
     redirect to("/figures/#{@figure.id}")
 
